9e0ced90605cc856c3e619fab232f250b9ed96b3
amrsin <amrsin@alu.iesserpis.org>
2019-12-19 08:55:02 +0100

    Excepcion por si el vector esta vacio

--- vectornew/src/vectornew/vector.java
+++ vectornew/src/vectornew/vector.java
@@ -9,6 +9,9 @@ public class vector {
 		 
 	}
       public static int max(int[] v) {
+    	  if (v.length == 0) {
+			throw new IllegalArgumentException();
+		}
          int max= v[0];
          for (int i = 1; i < v.length; i++) {
 			
@@ -21,8 +24,12 @@ public class vector {
       
       public static void ordenar(int[] v1) {
     	  
+    	  
     	  for (int i = 0; i < v1.length - 1; i++) {
 			for (int j = i + 1 ; j < v1.length; j++) {
+				if (v1.length == 0) {
+					 throw new IllegalArgumentException();
+				}
 				if (v1[i] > v1[j]) {
 					int aux = v1[i];
 					v1[i] = v1[j];
@@ -30,6 +37,5 @@ public class vector {
 				}
 			}
 		}
-	
    }
 }
--- vectornew/src/vectornew/vectorTest.java
+++ vectornew/src/vectornew/vectorTest.java
@@ -1,12 +1,20 @@
 package vectornew;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Vector;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class vectorTest {
 
 	@Test
-	void test() {
-		assertEquals(21, vector.max(new int []{14,21,12,9, 7}));	
+	void max() {
+		assertEquals(21, vector.max(new int []{14,21,12,9, 7}));
+		
+		//en caso de que el vector esta vacio 
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() -> vector.max(new int[] {}));
 	}
 	
 	@Test
@@ -14,6 +22,9 @@ class vectorTest {
 		int[] v1= {14, 21, 12, 7, 9};
 		vector.ordenar(v1);
 		assertArrayEquals(new int[] {7, 9, 12, 14, 21}, v1);
+		
+		//en caso de que el vector esta vacio 
+				Assertions.assertThrows(IllegalArgumentException.class, 
+						() -> vector.max(new int[] {}));
+			}
 	}
-}
-
