d0fa2937231ddcbeb2614d4a70a7e8ccf10cc1bd
amrsin <amrsin@alu.iesserpis.org>
2020-01-13 12:35:59 +0100

    Hola mundo con el nombre del user

---
+++ monodevelop/holamundo/holamundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace holamundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(" Introduzca su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+
+
+        }
+    }
+}
+
