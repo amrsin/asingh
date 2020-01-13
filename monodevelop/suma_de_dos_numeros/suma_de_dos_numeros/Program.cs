2407c027b7157270d2ee9e0f60f926bad780d198
amrsin <amrsin@alu.iesserpis.org>
2020-01-13 12:48:07 +0100

    Programa que suma dos numeros

---
+++ monodevelop/suma_de_dos_numeros/suma_de_dos_numeros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace suma_de_dos_numeros
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //variables
+            int num1;
+            int num2;
+
+            Console.Write("Introduzca el num1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca el num2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            //Operaciones
+
+            Console.Write("La suma de " + num1 + "+"+ num2 + " es " + (num1 + num2));
+        }
+    }
+}
