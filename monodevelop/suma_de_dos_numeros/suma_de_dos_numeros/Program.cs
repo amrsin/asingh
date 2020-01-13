f58516c0baf4b98a69d1245d906dd4068c4a6264
amrsin <amrsin@alu.iesserpis.org>
2020-01-13 12:56:19 +0100

    Realiza todas las operaciones

--- monodevelop/suma_de_dos_numeros/suma_de_dos_numeros/Program.cs
+++ monodevelop/suma_de_dos_numeros/suma_de_dos_numeros/Program.cs
@@ -7,17 +7,23 @@ namespace suma_de_dos_numeros
         public static void Main(string[] args)
         {
             //variables
-            int num1;
-            int num2;
+            double num1;
+            double num2;
 
             Console.Write("Introduzca el num1: ");
-            num1 = int.Parse(Console.ReadLine());
+            num1 = double.Parse(Console.ReadLine());
             Console.Write("Introduzca el num2: ");
-            num2 = int.Parse(Console.ReadLine());
+            num2 = double.Parse(Console.ReadLine());
 
             //Operaciones
 
-            Console.Write("La suma de " + num1 + "+"+ num2 + " es " + (num1 + num2));
+            Console.WriteLine("La Suma de " + num1 + "+"+ num2 + " es = " + (num1 + num2));
+            Console.WriteLine("La Resta de " + num1 + "-" + num2 + " es = " + (num1 - num2));
+            Console.WriteLine("La Multiplicación de " + num1 + "x" + num2 + " es =" + (num1 * num2));
+            Console.WriteLine("La División de " + num1 + "/" + num2 + " es = " + (num1 / num2));
+            Console.WriteLine("La Resto de " + num1 + "%" + num2 + " es = " + (num1 % num2));
+
         }
+
     }
 }
