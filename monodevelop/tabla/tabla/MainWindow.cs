896d342eaa585ad0d384907304e919e58043f6a1
amrsin <amrsin@alu.iesserpis.org>
2020-01-27 13:43:09 +0100

    Creación de tablas con bottones, al dar click cambia de color a azul, panel.cs codigo externo para llamar desde main

---
+++ monodevelop/tabla/tabla/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+using tabla;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        Panel panel = new Panel(vbox1);
+
+        /*Table table = new Table(3, 3, true);
+        int index = 0;
+           for (int row =0; row <3; row++) 
+             for (int colum =0; colum<3; colum++)
+            {
+                index++;
+                Button button = new Button();
+                table.Attach(button, (uint)colum, (uint)colum + 1, (uint)row, (uint)row + 1);
+                button.Label = "BUTTON " + index.ToString();
+                button.Clicked += delegate
+                {
+                    button.ModifyBg(StateType.Normal, new Gdk.Color(100, 200, 250));
+                };
+            }
+        vbox1.Add(table);
+        table.ShowAll();*/
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
---
+++ monodevelop/tabla/tabla/Panel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gtk;
+
+
+namespace tabla
+{
+    public class Panel
+    {
+        public Panel(VBox vbox1)
+         { 
+
+        Table table = new Table(3, 3, true);
+        int index = 0;
+           for (int row =0; row<3; row++) 
+             for (int colum =0; colum<3; colum++)
+            {
+                index++;
+                Button button = new Button();
+                table.Attach(button, (uint) colum, (uint) colum + 1, (uint) row, (uint)row + 1);
+                button.Label = "BUTTON " + index.ToString();
+                button.Clicked += delegate
+                {
+                    button.ModifyBg(StateType.Normal, new Gdk.Color(100, 200, 250));
+                };
+    }
+            vbox1.Add(table);
+            table.ShowAll();
+
+        }
+
+
+    }
+  }
