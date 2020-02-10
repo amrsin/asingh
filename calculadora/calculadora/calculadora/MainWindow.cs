34e8846c1e3ce975af8cf3a6cdb34b7e3cdaf4c4
amrsin <amrsin@alu.iesserpis.org>
2020-02-10 13:55:42 +0100

    Acabado el diseño y funcion a los botones de numero y C para eliminar una posicion, vacir para vaciar todo el contenido

---
+++ calculadora/calculadora/calculadora/MainWindow.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBdelClicked(object sender, EventArgs e)
+    {
+        pantalla.DeleteText (0, pantalla.Text.Length);
+
+    }
+
+    protected void OnBcClicked(object sender, EventArgs e)
+    {
+        pantalla.DeleteText(pantalla.Text.Length - 1, pantalla.Text.Length);
+
+    }
+
+    protected void OnB7Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "7");
+
+    }
+
+    protected void OnB8Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "8");
+
+    }
+
+    protected void OnB9Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "9");
+    }
+
+    protected void OnBdividirClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void OnB4Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "4");
+
+    }
+
+    protected void OnB5Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "5");
+
+    }
+
+    protected void OnB6Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "6");
+
+    }
+
+    protected void OnBmultiplicarClicked(object sender, EventArgs e)
+    {
+
+
+    }
+
+    protected void OnB1Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "1");
+
+    }
+
+    protected void OnB2Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "2");
+    }
+
+    protected void OnB3Clicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "3");
+    }
+
+    protected void OnBrestaClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void OnBpuntoClicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + ".");
+    }
+
+    protected void OnBceroClicked(object sender, EventArgs e)
+    {
+        String display = pantalla.Text.ToString();
+        pantalla.DeleteText(0, pantalla.Text.Length);
+        pantalla.InsertText(display + "0");
+    }
+
+    protected void OnBigualClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void OnBsumaClicked(object sender, EventArgs e)
+    {
+
+    }
+}
