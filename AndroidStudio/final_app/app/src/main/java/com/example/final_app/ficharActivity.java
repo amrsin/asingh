3dd41505dd7243cc5d0da3ba27fe2f61253a46fe
amrsin <amrsin@alu.iesserpis.org>
2020-04-27 10:50:41 +0200

    Empeazado a realizar estructura de la app, por demomento realizado la pagina de login y permite dejar entrar segun la lista de usuarios almacenados en archivo usr_pwd y si es valido entra en FicharActivity

---
+++ AndroidStudio/final_app/app/src/main/java/com/example/final_app/ficharActivity.java
@@ -0,0 +1,14 @@
+package com.example.final_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ficharActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fichar);
+    }
+}
